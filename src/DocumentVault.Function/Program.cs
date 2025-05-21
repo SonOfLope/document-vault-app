2d0f62826c21473ffce839b28340c7291bf5d09d
SonOfLope <jonathanlopez@hotmail.ca>
2025-05-21 15:00:24 -0400

    Add Application Insights telemetry and HTTP extension configuration

--- src/DocumentVault.Function/Program.cs
+++ src/DocumentVault.Function/Program.cs
@@ -1,4 +1,5 @@
 using DocumentVault.Function.Extensions;
+using Microsoft.Extensions.Logging.ApplicationInsights;
 
 namespace DocumentVault.Function
 {
@@ -7,14 +8,28 @@ namespace DocumentVault.Function
         public static void Main(string[] args)
         {
             var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
+                .ConfigureFunctionsWorkerDefaults(builder =>
+                {
+                    builder.UseDefaultWorkerMiddleware();
+                })
                 .ConfigureAppConfiguration(config =>
                 {
                     config.AddEnvironmentVariables();
+                    config.AddJsonFile("host.json", optional: false);
+                })
+                .ConfigureLogging((context, builder) =>
+                {
+                    builder.AddApplicationInsights(
+                        configureTelemetryConfiguration: (config) => 
+                            config.ConnectionString = context.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"],
+                        configureApplicationInsightsLoggerOptions: (options) => { }
+                    );
+                    builder.AddFilter<ApplicationInsightsLoggerProvider>("", LogLevel.Information);
                 })
                 .ConfigureServices((context, services) =>
                 {
                     services.AddFunctionServices(context.Configuration);
+                    services.AddApplicationInsightsTelemetryWorkerService();
                 })
                 .Build();
 
